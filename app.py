6a4d467fcfd718d9ee83d30c4a81c1c9207352b6
Ajay77290 <ajaynaidu3826@gmail.com>
2025-12-28 11:19:29 +0530

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("name")
+def home():
+    return "Hello, this is Ajay"
+
+@app.route("number")
+def health():
+    return "7729033826"
+
+    app.run(host="0.0.0.0", port=5000)
